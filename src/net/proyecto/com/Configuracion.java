ed4b576679c88825594d6bf58befee1e560cb141
Pedro López <thepekingst@gmail.com>
2016-02-08 19:08:07 +0100

    Todas las clases creadas y preparadas para empezar con Hibernate

--- src/net/proyecto/com/Configuracion.java
+++ src/net/proyecto/com/Configuracion.java
@@ -10,23 +10,51 @@ package net.proyecto.com;
 public class Configuracion {
     private int IdConfiguracion;
     private int IdPersona;
-    private Binary Imagen;
     private String Estado;
     private String Fondo;
     private Boolean Privado;
+	@Override
+	public String toString() {
+		return "Configuracion [IdConfiguracion=" + IdConfiguracion + ", IdPersona=" + IdPersona + ", Estado=" + Estado
+				+ ", Fondo=" + Fondo + ", Privado=" + Privado + "]";
+	}
+	public int getIdConfiguracion() {
+		return IdConfiguracion;
+	}
+	public void setIdConfiguracion(int idConfiguracion) {
+		IdConfiguracion = idConfiguracion;
+	}
+	public int getIdPersona() {
+		return IdPersona;
+	}
+	public void setIdPersona(int idPersona) {
+		IdPersona = idPersona;
+	}
+	public String getEstado() {
+		return Estado;
+	}
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+	public String getFondo() {
+		return Fondo;
+	}
+	public void setFondo(String fondo) {
+		Fondo = fondo;
+	}
+	public Boolean getPrivado() {
+		return Privado;
+	}
+	public void setPrivado(Boolean privado) {
+		Privado = privado;
+	}
+	public Configuracion(int idPersona, String estado, String fondo, Boolean privado) {
+		IdPersona = idPersona;
+		Estado = estado;
+		Fondo = fondo;
+		Privado = privado;
+	}
     
-    Public Configuracion(final int IdConfiguracion, 
-                            final int IdPersona, 
-                            final Binary Imagen, 
-                            final String Estado,
-                            final String Fondo,
-                            final Boolean Privado){
-                                
-        this.IdConfiguracion = IdConfiguracion;
-        this.IdPersona = IdPersona;
-        this.Imagen = Imagen;
-        this.Estado = Estado;
-        this.Fondo = Fondo;
-        this.Privado = privado;
-    }
+    
+    
 }
