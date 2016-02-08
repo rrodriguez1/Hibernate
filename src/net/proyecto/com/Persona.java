d67081d8271b680a07fd3a17b34f7f35b47cdaa8
Pedro López <thepekingst@gmail.com>
2016-02-08 18:29:35 +0100

    Creada classe Twitt

--- src/net/proyecto/com/Persona.java
+++ src/net/proyecto/com/Persona.java
@@ -1,8 +1,14 @@
 package net.proyecto.com;
 
+import java.sql.Date;
+
 public class Persona {
 
 	int IdPersona;
 	String Nombre;
 	String Apellidos;
+	String Direccion;
+	Date FechaNacimiento;
+	Date FechaAlta;
+	
 }
---
+++ src/net/proyecto/com/Twitt.java
@@ -0,0 +1,5 @@
+package net.proyecto.com;
+
+public class Twitt {
+
+}
